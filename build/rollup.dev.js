a9c848098adc0b1532c35f8791d6f93af59779a0
Arthur.O <270079245@qq.com>
2018-12-19 18:19:06 +0800

    tet

--- build/rollup.dev.js
+++ build/rollup.dev.js
@@ -1,14 +1,10 @@
 import clear from 'rollup-plugin-clear'
 import postcss from 'rollup-plugin-postcss'
-import babel from 'rollup-plugin-babel'
 import name from './name'
 
 export default [
     clear({
         targets: ['dist']
-    }),
-    babel({
-        exclude: 'node_modules',
     }),
     postcss({
         extract : `dist/css/${name}.css`
