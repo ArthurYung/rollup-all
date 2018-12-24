ccb96c1e1dcd5e80282bb689350ca856193190cd
Arthur Ou <270079245@qq.com>
2018-12-25 00:09:26 +0800

    postcss

--- build/config.base.js
+++ build/config.base.js
@@ -3,7 +3,10 @@ import resolve from 'rollup-plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
 import moduleName from './name';
 import filesize from 'rollup-plugin-filesize'
+import autoprefixer from 'autoprefixer'
+
 const TYPE = process.env.NODE_ENV
+const ENV = process.env.npm_lifecycle_event
 
 const baseConfig = {
 	input: TYPE === 'ts' ? 'src/index.ts' : 'src/index.js',
@@ -13,9 +16,26 @@ const baseConfig = {
 		}),
 		resolve(),
 		postcss({
-      extract : `dist/css/${moduleName}.css`
+      extract : `dist/css/${moduleName}.css`,
+			plugins: [
+				autoprefixer({
+						browsers: [
+							'ie >= 9',
+							'ie_mob >= 10',
+							'ff >= 30',
+							'chrome >= 34',
+							'safari >= 7',
+							'opera >= 23',
+							'ios >= 7',
+							'android >= 4.4',
+							'bb >= 10'
+						]
+				})
+			],
+			minimize: ENV !== 'start'
     }),
-    filesize()],
+    filesize()
+	]
 }
 
 export default baseConfig
