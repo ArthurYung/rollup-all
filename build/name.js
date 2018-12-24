8f5c7786160aceee3fd50f4bf77639aa130ca9ff
Arthur Ou <270079245@qq.com>
2018-12-24 12:41:23 +0800

    Update name.js

--- build/name.js
+++ build/name.js
@@ -3,7 +3,7 @@ import { name } from '../package.json'
 const cases = name.split('-')
 let CamelCase = ''
 if(cases.length === 1) CamelCase = cases[0]
-cases.map( (n, i) => {
+else cases.map( (n, i) => {
     i === 0 ? CamelCase += n : CamelCase += n.replace(/^./, x=>x.toUpperCase())
 })
 CamelCase
